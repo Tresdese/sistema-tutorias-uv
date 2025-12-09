d2e1c650cfeecd5d89220c93554d988cd9d12b95
Tresdese <super_diegobros@hotmail.com>
2025-12-08 23:11:14 -0600

    Aplicacion principal del proyecto modificada

--- src/main/java/com/sistematutoriascomp/sistematutorias/Sistematutorias.java
+++ src/main/java/com/sistematutoriascomp/sistematutorias/Sistematutorias.java
@@ -1,8 +1,26 @@
 package com.sistematutoriascomp.sistematutorias;
 
-public class Sistematutorias {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Sistematutorias extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent vistaRaiz = FXMLLoader.load(getClass().getResource("/FXMLInicioSesion.fxml"));
+            Scene escena = new Scene(vistaRaiz);
+            primaryStage.setTitle("x");
+            primaryStage.setScene(escena);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
